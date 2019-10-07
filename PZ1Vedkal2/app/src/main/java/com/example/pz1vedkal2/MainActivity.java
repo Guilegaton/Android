90f011a57db504485fd5e62625fcbf93f265d842
student <yulii.vedkal@nure.ua>
2019-10-07 17:59:01 +0300

    pz1

---
+++ PZ1Vedkal2/app/src/main/java/com/example/pz1vedkal2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.pz1vedkal2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
